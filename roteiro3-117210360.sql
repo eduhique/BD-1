d972b0777e27be42c55abde39a96325b9eca703e
eduhique <eduardo.henrique.silva@ccc.ufcg.edu.br>
2019-04-10 11:09:51 -0300

    kkk

--- roteiro3-117210360.sql
+++ roteiro3-117210360.sql
@@ -1,67 +1,65 @@
--- 
+--
 -- Criacao das Tabelas
--- 
+--
 
 CREATE TYPE ESTADO_NE AS ENUM ('PB', 'PE', 'RN', 'BA', 'CE', 'PI','SE', 'AL','MA');
-CREATE TYPE funcao_fun AS ENUM ('FARMACEUTICO', 'VENDEDOR', 'ENTREGADOR', 'CAIXA', 'ADMINISTRADOR');
-CREATE TYPE adress_tipo AS  ENUM ('RESIDENCIA', 'TRABALHO', 'OUTRO');
+CREATE TYPE FUNCAO_FUN AS ENUM ('FARMACEUTICO', 'VENDEDOR', 'ENTREGADOR', 'CAIXA', 'ADMINISTRADOR');
+CREATE TYPE ADDRESS_TIPO AS  ENUM ('RESIDENCIA', 'TRABALHO', 'OUTRO');
 
 CREATE TABLE funcionarios(
     cpf CHAR(11) NOT NULL,
     nome TEXT NOT NULL,
-    funcao funcao_fun NOT NULL,
+    funcao FUNCAO_FUN NOT NULL,
     farmaciaID BIGINT,
-    gerente BOOLEAN NOT NULL,
     CONSTRAINT funcionarios_cpf_pkey PRIMARY KEY(cpf),
     CONSTRAINT funcionarios_chk_cpf_valido CHECK(LENGTH(cpf) = 11),
     CONSTRAINT funcionarios_chk_farmciaID_valido CHECK(farmaciaID >0),
-    CONSTRAINT funcionarios_gerente_ex EXCLUDE USING gist(farmaciaID with=) WHERE (gerente = true),
-    CONSTRAINT funcionarios_gerente_chk CHECK((farmaciaID IS NOT NULL AND gerente = true AND (funcao = 'FARMACEUTICO' OR funcao = 'ADMINISTRADOR')) OR (gerente = false))
+    CONSTRAINT funcionarios_funcao_uni UNIQUE(cpf, funcao)
 );
 
 CREATE TABLE farmacias(
-    fa_id BIGINT,
+    fa_id SERIAL,
     nome VARCHAR(20) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
     cidade VARCHAR(20) NOT NULL,
     estado ESTADO_NE NOT NULL,
     gerente CHAR(11) NOT NULL,
-    gerente_fun funcao_fun NOT NULL,
+    gerente_fun FUNCAO_FUN NOT NULL,
     tipo_loja CHAR(1) NOT NULL,
     CONSTRAINT farmacias_id_pkey PRIMARY KEY(fa_id),
     CONSTRAINT farmacias_chk_fa_ID_valido CHECK(fa_id >0),
     CONSTRAINT farmacias_bairro_un UNIQUE(bairro),
     CONSTRAINT farmacias_tipo_ex EXCLUDE USING gist(tipo_loja with=) WHERE (tipo_loja = 'S'),
     CONSTRAINT farmacias_tipo_chk CHECK(tipo_loja = 'S' OR tipo_loja = 'F'),
-    CONSTRAINT farmacias_gerente_fkey FOREIGN KEY (gerente) REFERENCES funcionarios(cpf),
-    CONSTRAINT farmacias_gerente_fun_fkey FOREIGN KEY (gerente_fun) REFERENCES funcionarios(funcao),
+    CONSTRAINT farmacias_gerente_fkey FOREIGN KEY (gerente, gerente_fun) REFERENCES funcionarios(cpf,funcao),
     CONSTRAINT farmacias_gerente_fun_chk CHECK(gerente_fun = 'FARMACEUTICO' OR gerente_fun = 'ADMINISTRADOR')
 );
 
 ALTER TABLE funcionarios ADD CONSTRAINT funcionarios_farmaciaID_fkey FOREIGN KEY(farmaciaID) REFERENCES farmacias(fa_id);
 
 CREATE TABLE medicamentos(
-    medID BIGINT NOT NULL,
+    medID SERIAL,
     nome VARCHAR(50) NOT NULL,
     prin_ativo VARCHAR(50) NOT NULL,
-    venda_exclusiva BOOLEAN NOT NULL UNIQUE,
+    venda_exclusiva BOOLEAN NOT NULL,
     valor NUMERIC NOT NULL,
+    CONSTRAINT medicamentos_venda_uni UNIQUE(medID, venda_exclusiva),
     CONSTRAINT medicamentos_medID_pkey PRIMARY KEY(medID),
-    CONSTRAINT medicamentos_valor_chk CHECK(valor >= 0)  
+    CONSTRAINT medicamentos_valor_chk CHECK(valor >= 0)
 );
 
 CREATE TABLE clientes(
-    clienteID BIGINT NOT NULL,
+    clienteID SERIAL,
     nome VARCHAR(50) NOT NULL,
     data_nasc date NOT NULL,
     CONSTRAINT clientes_clienteID_pkey PRIMARY KEY(clienteID),
-    CONSTRAINT clientes_idade_min_chk CHECK((extract(year from age(date '1997-07-21'))) >= 18)
+    CONSTRAINT clientes_idade_min_chk CHECK((extract(year from age(data_nasc))) >= 18)
 );
 
 CREATE TABLE enderecos(
-    endeID BIGINT NOT NULL,
+    endeID SERIAL,
     clienteID BIGINT NOT NULL,
-    tipo_endereco adress_tipo NOT NULL,
+    tipo_endereco ADDRESS_TIPO NOT NULL,
     estado ESTADO_NE NOT NULL,
     cidade VARCHAR(20) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
@@ -71,26 +69,24 @@ CREATE TABLE enderecos(
 );
 
 CREATE TABLE vendas(
-    venID BIGINT NOT NULL,
+    venID SERIAL,
     produto BIGINT NOT NULL,
     produto_receita BOOLEAN NOT NULL,
     clienteID BIGINT,
     valor NUMERIC NOT NULL,
     vendedor CHAR(11) NOT NULL,
-    vendedor_fun funcao_fun NOT NULL,
+    vendedor_fun FUNCAO_FUN NOT NULL,
     CONSTRAINT vendas_venID_pkey PRIMARY KEY(venID),
-    CONSTRAINT vendas_produto_fkey FOREIGN KEY(produto) REFERENCES medicamentos(medID) ON DELETE RESTRICT,
-    CONSTRAINT vendas_produto_receita_fkey FOREIGN KEY(produto_receita) REFERENCES medicamentos(venda_exclusiva) ON DELETE RESTRICT,
+    CONSTRAINT vendas_produto_receita_fkey FOREIGN KEY(produto, produto_receita) REFERENCES medicamentos(medID, venda_exclusiva) ON DELETE RESTRICT,
     CONSTRAINT vendas_clienteID_fkey FOREIGN KEY(clienteID) REFERENCES clientes(clienteID),
-    CONSTRAINT vendas_vendedor_fkey FOREIGN KEY(vendedor) REFERENCES funcionarios(cpf) ON DELETE RESTRICT,
-    CONSTRAINT vendas_vendedor_fun_fkey FOREIGN KEY(vendedor_fun) REFERENCES funcionarios(funcao) ON DELETE RESTRICT,
-    CONSTRAINT vendas_receita_chk CHECK(clienteID IS NOT NULL AND produto_receita = true),
+    CONSTRAINT vendas_vendedor_fkey FOREIGN KEY(vendedor, vendedor_fun) REFERENCES funcionarios(cpf, funcao) ON DELETE RESTRICT,
+    CONSTRAINT vendas_receita_chk CHECK (NOT(clienteID IS NULL AND produto_receita = true)),
     CONSTRAINT vendas_valor_chk CHECK(valor >= 0),
     CONSTRAINT vendas_vendedor_chk CHECK(vendedor_fun = 'VENDEDOR')
 );
 
 CREATE TABLE entregas(
-    entregaID BIGINT NOT NULL,
+    entregaID SERIAL,
     vendaID BIGINT NOT NULL,
     clienteID BIGINT NOT NULL,
     enderecoID BIGINT NOT NULL,
@@ -100,12 +96,12 @@ CREATE TABLE entregas(
     CONSTRAINT entregas_enderecoID_fkey FOREIGN KEY(enderecoID) REFERENCES enderecos(endeID)
 );
 
--- 
+--
 -- COMANDOS ADICIONAIS
--- 
+--
 
 -- Devem ser executados com sucesso
--- 
+--
 
 INSERT INTO funcionarios VALUES ('12734805000', 'Carlos Wilson', 'FARMACEUTICO', null, false);
 INSERT INTO funcionarios VALUES ('12734805001', 'Pedro Santos', 'FARMACEUTICO', null, false);
