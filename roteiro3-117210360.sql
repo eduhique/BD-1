a1d7468c507ef49945346e7d1ec4a8ed7a892bac
eduhique <eduardo.henrique.silva@ccc.ufcg.edu.br>
2019-04-11 02:18:57 -0300

    falta apenas os exemplos

--- roteiro3-117210360.sql
+++ roteiro3-117210360.sql
@@ -10,7 +10,7 @@ CREATE TABLE funcionarios(
     cpf CHAR(11) NOT NULL,
     nome TEXT NOT NULL,
     funcao FUNCAO_FUN NOT NULL,
-    farmaciaID BIGINT,
+    farmaciaID INTEGER,
     CONSTRAINT funcionarios_cpf_pkey PRIMARY KEY(cpf),
     CONSTRAINT funcionarios_chk_cpf_valido CHECK(LENGTH(cpf) = 11),
     CONSTRAINT funcionarios_chk_farmciaID_valido CHECK(farmaciaID >0),
@@ -19,7 +19,7 @@ CREATE TABLE funcionarios(
 
 CREATE TABLE farmacias(
     fa_id SERIAL,
-    nome VARCHAR(20) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
     cidade VARCHAR(20) NOT NULL,
     estado ESTADO_NE NOT NULL,
@@ -31,11 +31,12 @@ CREATE TABLE farmacias(
     CONSTRAINT farmacias_bairro_un UNIQUE(bairro),
     CONSTRAINT farmacias_tipo_ex EXCLUDE USING gist(tipo_loja with=) WHERE (tipo_loja = 'S'),
     CONSTRAINT farmacias_tipo_chk CHECK(tipo_loja = 'S' OR tipo_loja = 'F'),
-    CONSTRAINT farmacias_gerente_fkey FOREIGN KEY (gerente, gerente_fun) REFERENCES funcionarios(cpf,funcao),
-    CONSTRAINT farmacias_gerente_fun_chk CHECK(gerente_fun = 'FARMACEUTICO' OR gerente_fun = 'ADMINISTRADOR')
+    CONSTRAINT farmacias_gerente_fkey FOREIGN KEY (gerente, gerente_fun) REFERENCES funcionarios(cpf,funcao) ON DELETE RESTRICT,
+    CONSTRAINT farmacias_gerente_fun_chk CHECK(gerente_fun = 'FARMACEUTICO' OR gerente_fun = 'ADMINISTRADOR'),
+    CONSTRAINT farmacias_gerente_un UNIQUE(gerente, fa_id)
 );
 
-ALTER TABLE funcionarios ADD CONSTRAINT funcionarios_farmaciaID_fkey FOREIGN KEY(farmaciaID) REFERENCES farmacias(fa_id);
+ALTER TABLE funcionarios ADD CONSTRAINT funcionarios_farmaciaID_fkey FOREIGN KEY(cpf, farmaciaID) REFERENCES farmacias(gerente, fa_id);
 
 CREATE TABLE medicamentos(
     medID SERIAL,
@@ -58,7 +59,7 @@ CREATE TABLE clientes(
 
 CREATE TABLE enderecos(
     endeID SERIAL,
-    clienteID BIGINT NOT NULL,
+    clienteID INTEGER NOT NULL,
     tipo_endereco ADDRESS_TIPO NOT NULL,
     estado ESTADO_NE NOT NULL,
     cidade VARCHAR(20) NOT NULL,
@@ -70,9 +71,9 @@ CREATE TABLE enderecos(
 
 CREATE TABLE vendas(
     venID SERIAL,
-    produto BIGINT NOT NULL,
+    produto INTEGER NOT NULL,
     produto_receita BOOLEAN NOT NULL,
-    clienteID BIGINT,
+    clienteID INTEGER,
     valor NUMERIC NOT NULL,
     vendedor CHAR(11) NOT NULL,
     vendedor_fun FUNCAO_FUN NOT NULL,
@@ -87,9 +88,9 @@ CREATE TABLE vendas(
 
 CREATE TABLE entregas(
     entregaID SERIAL,
-    vendaID BIGINT NOT NULL,
-    clienteID BIGINT NOT NULL,
-    enderecoID BIGINT NOT NULL,
+    vendaID INTEGER NOT NULL,
+    clienteID INTEGER NOT NULL,
+    enderecoID INTEGER NOT NULL,
     CONSTRAINT entregas_entregaID_pkey PRIMARY KEY(entregaID),
     CONSTRAINT entregas_vendaID_fkey FOREIGN KEY(vendaID) REFERENCES vendas(venID),
     CONSTRAINT entregas_clienteID_fkey FOREIGN KEY(clienteID) REFERENCES clientes(clienteID),
@@ -103,7 +104,19 @@ CREATE TABLE entregas(
 -- Devem ser executados com sucesso
 --
 
-INSERT INTO funcionarios VALUES ('12734805000', 'Carlos Wilson', 'FARMACEUTICO', null, false);
-INSERT INTO funcionarios VALUES ('12734805001', 'Pedro Santos', 'FARMACEUTICO', null, false);
-INSERT INTO funcionarios VALUES ('12734805013', 'Ana Paula', 'ADMINSTRADOR', null, false);
-INSERT INTO funcionarios VALUES ('12734805001', 'Carla Silva', 'VENDEDOR', null, false);
+INSERT INTO funcionarios VALUES ('12734805000', 'Carlos Wilson', 'FARMACEUTICO', null),
+    ('12734805001', 'Pedro Santos', 'FARMACEUTICO', null),
+    ('12734805013', 'Ana Paula', 'ADMINISTRADOR', null),
+    ('12734805021', 'Carla Silva', 'VENDEDOR', null),
+    ('12734804002', 'Mario Batista', 'ENTREGADOR', null),
+    ('12734807001', 'Fernanda Barbosa', 'ADMINISTRADOR', null),
+    ('12734805002', 'Joao Paulo', 'ENTREGADOR', null);
+
+INSERT INTO farmacias VALUES (DEFAULT, 'REDEPHARMA Unidade Manaira', 'Manaira', 'João Pessoa', 'PB', '12734805000', 'FARMACEUTICO', 'S'),
+    (DEFAULT,'REDEPHARMA Unidade Geisel', 'geisel', 'João Pessoa', 'PB', '12734805013', 'ADMINISTRADOR', 'F'),
+    (DEFAULT,'PAGUE MENOS Unidade Prata', 'prata', 'Campina Grande', 'PB', '12734805001', 'FARMACEUTICO', 'F');
+
+UPDATE funcionarios SET farmaciaID = 1 WHERE cpf = '12734805000';
+UPDATE funcionarios SET farmaciaID = 2 WHERE cpf = '12734805013';
+UPDATE funcionarios SET farmaciaID = 3 WHERE cpf = '12734805001';
+
