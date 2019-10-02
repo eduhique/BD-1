93d4620a276ec0ac3bc30945a5c2d1ddd9d3881f
grabowski74 <matheus.araujo@ccc.ufcg.edu.br>
2019-10-01 21:27:15 -0300

    melhorando a identacao

--- roteiro4-Eduardo-117210360.sql
+++ roteiro4-Eduardo-117210360.sql
@@ -1,53 +1,83 @@
 -- Q1
-SELECT * FROM department;
+SELECT * 
+FROM department;
 
 -- Q2
-SELECT * FROM dependent;
+SELECT * 
+FROM dependent;
 
 -- Q3
-SELECT * FROM dept_locations;
+SELECT * 
+FROM dept_locations;
 
 -- Q4
-SELECT * FROM employee;
+SELECT * 
+FROM employee;
 
 -- Q5
-SELECT * FROM project;
+SELECT * 
+FROM project;
 
 -- Q6
-SELECT * FROM works_on;
+SELECT * 
+FROM works_on;
 
 -- Q7
-SELECT fname, lname FROM employee WHERE sex = 'M';
+SELECT fname, lname 
+FROM employee 
+WHERE sex = 'M';
 
 -- Q8
-SELECT fname FROM employee WHERE sex = 'M' AND superssn IS NULL;
+SELECT fname 
+FROM employee 
+WHERE sex = 'M' AND superssn IS NULL;
 
 -- Q9
-SELECT E.fname AS funcionario, S.Fname AS supervisor FROM employee AS E, employee AS S WHERE E.superssn IS NOT NULL AND E.superssn = S.ssn;
+SELECT E.fname AS funcionario, S.Fname AS supervisor 
+FROM employee AS E, employee AS S 
+WHERE E.superssn IS NOT NULL AND E.superssn = S.ssn;
 
 -- Q10
-SELECT E.fname AS funcionario FROM employee AS E, employee AS S  WHERE E.superssn IS NOT NULL AND E.superssn = S.ssn AND S.fname = 'Franklin';
+SELECT E.fname AS funcionario 
+FROM employee AS E, employee AS S  
+WHERE E.superssn IS NOT NULL AND E.superssn = S.ssn AND S.fname = 'Franklin';
 
 -- Q11
-SELECT D.dname AS departamento, L.dlocation AS localizacao FROM department AS D, dept_locations AS L WHERE D.dnumber = L.dnumber;
+SELECT D.dname AS departamento, L.dlocation AS localizacao 
+FROM department AS D, dept_locations AS L 
+WHERE D.dnumber = L.dnumber;
 
 -- Q12
-SELECT D.dname AS departamento FROM department AS D, dept_locations AS L WHERE D.dnumber = L.dnumber AND dlocation LIKE 'S%';
+SELECT D.dname AS departamento 
+FROM department AS D, dept_locations AS L 
+WHERE D.dnumber = L.dnumber AND dlocation LIKE 'S%';
 
 -- Q13
-SELECT E.fname, E.lname, D.dependent_name FROM employee AS E, dependent AS D WHERE E.ssn = D.essn;
+SELECT E.fname, E.lname, D.dependent_name 
+FROM employee AS E, dependent AS D 
+WHERE E.ssn = D.essn;
 
 -- Q14
-SELECT (fname || ' ' || minit || '. ' || lname) AS full_name, salary from employee WHERE salary > 50000;
+SELECT (fname || ' ' || minit || '. ' || lname) AS full_name, salary 
+FROM employee 
+WHERE salary > 50000;
 
 -- Q15
-SELECT P.pname, D.dname FROM department AS D, project AS P WHERE P.dnum=D.dnumber;
+SELECT P.pname, D.dname 
+FROM department AS D, project AS P 
+WHERE P.dnum=D.dnumber;
 
 -- Q16
-SELECT P.pname AS Projeto_nome, E.fname AS gerente FROM department AS D, project AS P, employee AS E WHERE P.dnum=D.dnumber AND P.pnumber > 30 AND D.mgrssn = E.ssn;
+SELECT P.pname AS Projeto_nome, E.fname AS gerente 
+FROM department AS D, project AS P, employee AS E 
+WHERE P.dnum=D.dnumber AND P.pnumber > 30 AND D.mgrssn = E.ssn;
 
 -- Q17
-SELECT P.pname AS Projeto_nome, E.fname AS funcionario FROM project AS P, employee AS E, works_on AS W WHERE E.ssn = w.essn AND w.pno = P.pnumber;
+SELECT P.pname AS Projeto_nome, E.fname AS funcionario 
+FROM project AS P, employee AS E, works_on AS W 
+WHERE E.ssn = w.essn AND w.pno = P.pnumber;
 
 -- Q18
-SELECT E.fname, D.dependent_name, D.relationship FROM employee AS E, dependent AS D, works_on as W WHERE E.ssn = W.essn AND W.pno = 91 AND E.ssn = D.essn;
+SELECT E.fname, D.dependent_name, D.relationship 
+FROM employee AS E, dependent AS D, works_on as W 
+WHERE E.ssn = W.essn AND W.pno = 91 AND E.ssn = D.essn;
